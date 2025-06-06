2e884d3fbecb4efc4ff8f1526e884df2ef4a04fe
Reetu Khulbe <111274022+rtkhulbe@users.noreply.github.com>
2025-06-06 19:17:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.pyplot(fig1)
 
 # Attrition based on gender (Seaborn/Matplotlib)
 fig2, ax2 = plt.subplots(figsize=(10,6))
-sns.countplot(data=filtered_df, x="Gender", hue="Attrition", palette="purples", ax=ax2)
+sns.countplot(data=filtered_df, x="Gender", hue="Attrition", color ="purple", ax=ax2)
 st.pyplot(fig2)
 
 # Distribution of business travel (Matplotlib Pie Chart)
